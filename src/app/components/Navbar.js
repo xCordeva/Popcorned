b60a7b5abfdb727160842a81fdd57499baf4456d
Cordeva <ahmed.abdelaaall@gmail.com>
2024-05-14 17:29:47 +0300

    changed the grey shade i used for navbar and added a padding for it

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -19,38 +19,40 @@ export default function Navbar() {
   const { user } = useAuth();
   console.log(user);
   return (
-    <div className="navbar">
-      <div className="nav-logo">
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/popcorned-x.appspot.com/o/popcorned.png?alt=media&token=db91cd2d-06cd-4808-bb3b-5548b0e03762"
-          alt="popcorned-logo"
-        />
-      </div>
-      <div className="user-info">
-        <div className="favorites">
-          <FontAwesomeIcon icon={faHeart} />
-          <p>Favourites</p>
-        </div>
-        <div
-          className={userPopupClicked ? "user user-backgrounded" : "user"}
-          onClick={() => {
-            toggleUserPopup();
-          }}
-        >
-          <FontAwesomeIcon icon={faCircleUser} />
-          <p>
-            {user &&
-              user.displayName &&
-              user.displayName.substring(0, user.displayName.indexOf(" "))}
-          </p>
-
-          <FontAwesomeIcon
-            className={userPopupClicked ? "user-arrow-up" : "user-arrow-down"}
-            icon={faCaretDown}
+    <div className="navbar-container">
+      <div className="navbar">
+        <div className="nav-logo">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/popcorned-x.appspot.com/o/popcorned.png?alt=media&token=db91cd2d-06cd-4808-bb3b-5548b0e03762"
+            alt="popcorned-logo"
           />
         </div>
+        <div className="user-info">
+          <div className="favorites">
+            <FontAwesomeIcon icon={faHeart} />
+            <p>Watchlist</p>
+          </div>
+          <div
+            className={userPopupClicked ? "user user-backgrounded" : "user"}
+            onClick={() => {
+              toggleUserPopup();
+            }}
+          >
+            <FontAwesomeIcon icon={faCircleUser} />
+            <p>
+              {user &&
+                user.displayName &&
+                user.displayName.substring(0, user.displayName.indexOf(" "))}
+            </p>
+
+            <FontAwesomeIcon
+              className={userPopupClicked ? "user-arrow-up" : "user-arrow-down"}
+              icon={faCaretDown}
+            />
+          </div>
+        </div>
+        {userPopupClicked && <PopupUser></PopupUser>}
       </div>
-      {userPopupClicked && <PopupUser></PopupUser>}
     </div>
   );
 }
