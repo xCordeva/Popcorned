ae541826f0a0e86c77c6ab63fe3ad474429ad376
Cordeva <ahmed.abdelaaall@gmail.com>
2024-06-06 23:36:13 +0300

    made the search results show even if the user refreshes the page

--- src/app/search/title/[searchTerm]/page.js
+++ src/app/search/title/[searchTerm]/page.js
@@ -1,22 +1,91 @@
 "use client";
+import { useEffect } from "react";
 import Navbar from "@/app/components/Navbar";
-import { useSelector } from "react-redux";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+
+import "@/css/SearchPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleInfo,
+  faStar,
+  faSquarePlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { userSearchInput, fetchAll } from "@/features/UserSearch";
 
 const SearchPage = ({ params }) => {
+  const dispatch = useDispatch();
+
+  const searchInput = params.searchTerm;
+
   const SearchResults = useSelector(
     (state) => state.UserSearch.combinedResults
   );
   const userSearch = useSelector((state) => state.UserSearch.searchInput);
-  const { searchTerm } = params;
-  console.log(userSearch);
+
+  useEffect(() => {
+    dispatch(userSearchInput(searchInput));
+    dispatch(fetchAll(searchInput));
+  }, [searchInput, userSearch, dispatch]);
+
+  if (!searchInput && !userSearch) {
+    return (
+      <div>
+        <Navbar />
+        <div className="no-valid-search">
+          <h1>This is not a valid search</h1>
+          <Link href={`/`}>Back to Homepage</Link>
+        </div>
+      </div>
+    );
+  }
+  console.log(SearchResults);
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
       <div className="search-page">
-        <h1>Showing results for {searchTerm}</h1>
+        <h1>Showing results for {userSearch}</h1>
         {SearchResults.map((result) => (
           <div className="search-result" key={result.id}>
-            {result.title}
+            <img
+              src={
+                result.poster_path || result.profile_path
+                  ? result.poster_path
+                    ? `https://image.tmdb.org/t/p/w500${result.poster_path}`
+                    : `https://image.tmdb.org/t/p/w500${result.profile_path}`
+                  : "https://firebasestorage.googleapis.com/v0/b/popcorned-x.appspot.com/o/no-image-avaiable.jpg?alt=media&token=f01f2f4a-c8db-4e5f-8f7f-c920219a77fd"
+              }
+              alt={result.title + ` Poster`}
+            />
+            <div className="result-details">
+              <div className="title-rate">
+                <h1>{result.title ? result.title : result.name}</h1>
+                <div className="rating">
+                  {result.vote_average && (
+                    <p>
+                      <FontAwesomeIcon icon={faStar} />
+                      {result.vote_average.toFixed(1)}
+                      <span>/10</span>
+                    </p>
+                  )}
+                </div>
+              </div>
+              {(result.release_date || result.first_air_date) && (
+                <h2>
+                  {result.release_date
+                    ? result.release_date.split("-")[0]
+                    : result.first_air_date.split("-")[0]}
+                </h2>
+              )}
+              <Link href={"/movie-details"}>
+                Show More Info
+                <FontAwesomeIcon icon={faCircleInfo} />
+              </Link>
+              <Link href={"/"}>
+                Add to Watch List
+                <FontAwesomeIcon icon={faSquarePlus} />
+              </Link>
+            </div>
           </div>
         ))}
       </div>
@@ -25,5 +94,3 @@ const SearchPage = ({ params }) => {
 };
 
 export default SearchPage;
-
-// make it only work when the user serach not when someone type in the url
