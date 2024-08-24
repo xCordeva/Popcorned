6a2158338f6fe6adb1113546f4f48c316ae9f02e
Cordeva <ahmed.abdelaaall@gmail.com>
2024-08-24 22:20:09 +0300

    added the title to the layouts.js

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,9 +1,9 @@
 import "@/css/auth.css";
 
 export const metadata = {
-  title: "Popcorned | Online Planner & Organizer",
+  title: "Popcorned | Your Ultimate Destination for Movies & TV Shows",
   description:
-    "Calendizer is an online planner and organizer to help you manage your schedule and tasks efficiently. Stay organized and productive with Calendizer.",
+    "Discover a world of entertainment with Popcorned. Explore the latest movies, TV shows, and celebrity news, create your personalized watchlist, and dive deep into reviews and ratings. Stay updated with the hottest trends and what's coming soon, all in one place. Your journey into the cinematic universe starts here!",
 };
 
 export default function RootLayout({ children }) {
---
+++ src/app/account-settings/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Account Settings | Popcorned",
+  description:
+    "Manage your Popcorned account settings, update personal information, change your password, and adjust your preferences.",
+};
+
+export default function RootLayout({ children }) {
+  return <div>{children}</div>;
+}
