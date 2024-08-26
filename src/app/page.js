ba2bc4628c7f23242b7f83a0d7d4134a03775277
Cordeva <ahmed.abdelaaall@gmail.com>
2024-08-26 19:39:51 +0300

    made  a max width for the main page

--- src/app/page.js
+++ src/app/page.js
@@ -18,26 +18,28 @@ export default function Home() {
     <div>
       <Navbar></Navbar>
       <MainPageSearch></MainPageSearch>
-      <div className="main-page-carousel">
-        <h1 id="trending-movies">Trending Movies</h1>
-        <TrendingTitles type={"movie"}></TrendingTitles>
-      </div>
+      <div className="main-page">
+        <div className="main-page-carousel">
+          <h1 id="trending-movies">Trending Movies</h1>
+          <TrendingTitles type={"movie"}></TrendingTitles>
+        </div>
 
-      <div className="main-page-carousel">
-        <h1 id="trending-tv-shows">Trending Tv Shows</h1>
-        <TrendingTitles type={"tv"}></TrendingTitles>
-      </div>
-      <div className="main-page-carousel">
-        <h1 id="top-movies">Top Rated Movies</h1>
-        <TopRatedTitles type={"movie"}></TopRatedTitles>
-      </div>
-      <div className="main-page-carousel">
-        <h1 id="top-tv-shows">Top Rated Tv Shows</h1>
-        <TopRatedTitles type={"tv"}></TopRatedTitles>
-      </div>
-      <div className="main-page-carousel">
-        <h1 id="from-watchlist">From Your Watchlist</h1>
-        <FromYourWatchlist></FromYourWatchlist>
+        <div className="main-page-carousel">
+          <h1 id="trending-tv-shows">Trending Tv Shows</h1>
+          <TrendingTitles type={"tv"}></TrendingTitles>
+        </div>
+        <div className="main-page-carousel">
+          <h1 id="top-movies">Top Rated Movies</h1>
+          <TopRatedTitles type={"movie"}></TopRatedTitles>
+        </div>
+        <div className="main-page-carousel">
+          <h1 id="top-tv-shows">Top Rated Tv Shows</h1>
+          <TopRatedTitles type={"tv"}></TopRatedTitles>
+        </div>
+        <div className="main-page-carousel">
+          <h1 id="from-watchlist">From Your Watchlist</h1>
+          <FromYourWatchlist></FromYourWatchlist>
+        </div>
       </div>
       <Footer />
       {showSignInMessagePopup && <SignInMessage></SignInMessage>}
